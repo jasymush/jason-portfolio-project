c7743af069e27a686ad0350f98457c1c32444a5d
Jason Mugo <93824951+jasymush@users.noreply.github.com>
2024-11-19 14:04:27 +0300

    Add files via upload

---
+++ Portfolio-Project-main/js/script.js
@@ -0,0 +1,28 @@
+// Smooth scrolling to sections
+function scrollToSection(sectionId) {
+    const target = document.getElementById(sectionId);
+    window.scrollTo({
+        top: target.offsetTop,
+        behavior: "smooth"
+    });
+}
+
+// Form submission message
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for your message. We will be in touch soon!");
+});
+
+function showsidebar(){
+    const sidebar=document.querySelector('.sidebar')
+    sidebar.style.display='flex'
+}
+function hidesidebar(){
+    const sidebar=document.querySelector('.sidebar')
+    sidebar.style.display='none'
+}
+
+document.getElementById("rform").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Thank you for completing your application. We will be in touch soon!");
+});
---
+++ Portfolio-Project-main/js/script1.js
@@ -0,0 +1,3 @@
+document.getElementById("subbtn").addEventListener("click", () => {
+    alert("Thank you for completing your application. We will be in touch soon!");
+});
